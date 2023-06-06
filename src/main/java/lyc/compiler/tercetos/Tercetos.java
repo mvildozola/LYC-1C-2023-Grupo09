eb361905267407e68e61841006ef58832fea5494
Mariano <mariano_11m@hotmail.com>
2023-06-06 01:07:18 -0300

    Tercetos

--- src/main/java/lyc/compiler/tercetos/Tercetos.java
+++ src/main/java/lyc/compiler/tercetos/Tercetos.java
@@ -3,8 +3,22 @@ package lyc.compiler.tercetos;
 import java.util.ArrayList;
 
 public class Tercetos {
+   ArrayList<Terceto> tercetos;
+   private static Tercetos terceto;
 
-    ArrayList<Terceto> tercetos = new ArrayList<Terceto>();
+    private Tercetos() {
+      this.tercetos = new ArrayList<Terceto>(); 
+      System.out.println("Inicializando Tercetos");
+    }
+
+    public static Tercetos getSingletonInstance() {
+        if (terceto == null){
+          terceto = new Tercetos();
+        }
+        return terceto;
+    }
+
+    //ArrayList<Terceto> tercetos = new ArrayList<Terceto>();
 
     public Integer create(String param1, String param2, String param3) {
         tercetos.add(new Terceto(param1, param2, param3));
@@ -13,6 +27,11 @@ public class Tercetos {
 
     public Integer create(String param1, Integer param2, Integer param3) {
       tercetos.add(new Terceto(param1, "[" + param2 + "]", "[" + param3 + "]"));
+      return tercetos.size();
+    }
+
+    public Integer create(String param1, Integer param2, String param3) {
+      tercetos.add(new Terceto(param1, "[" + param2 + "]", param3 ));
       return tercetos.size();
     }
 
@@ -74,5 +93,18 @@ public class Tercetos {
             row.terceto()
           ));
         }
+    }
+
+    @Override
+    public String toString() {
+      int index = 1;
+      String result = "";
+      for (Terceto row : tercetos) {
+        result += 
+        String.format("[%s] %s",
+        (index++), row.terceto()
+        );
+      }
+      return result;
     }
 }
